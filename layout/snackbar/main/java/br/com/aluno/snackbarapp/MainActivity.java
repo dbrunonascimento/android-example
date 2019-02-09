2549285b780af2ccf7a83db8f6f1a3ad63045d14
unknown <Treinamento@administrador8.DTRAINNING.local>
2019-02-09 11:54:13 -0200

    Outros

---
+++ layout/snackbar/main/java/br/com/aluno/snackbarapp/MainActivity.java
@@ -0,0 +1,49 @@
+package br.com.aluno.snackbarapp;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    View.OnClickListener mOnClickListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id
+                .coordinatorLayout);
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Snackbar snackbar = Snackbar
+                            .make(coordinatorLayout, "Este é o snackbar", Snackbar.LENGTH_LONG)
+                            .setAction("Desfazer", mOnClickListener);
+                    snackbar.setActionTextColor(Color.RED);
+                    View snackbarView = snackbar.getView();
+                    snackbarView.setBackgroundColor(Color.DKGRAY);
+                    TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                    textView.setTextColor(Color.YELLOW);
+                    snackbar.show();
+                }
+            }
+        });
+
+        mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkBox.setChecked(false);
+            }
+        };
+    }
+}
