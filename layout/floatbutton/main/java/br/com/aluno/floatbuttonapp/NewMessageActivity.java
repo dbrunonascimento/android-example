218a2eb947fd2e7ec18ca939b795b5ac94959234
unknown <Treinamento@administrador8.DTRAINNING.local>
2019-02-09 10:34:49 -0200

    examplo float button

---
+++ layout/floatbutton/main/java/br/com/aluno/floatbuttonapp/NewMessageActivity.java
@@ -0,0 +1,22 @@
+package br.com.aluno.floatbuttonapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+
+
+public class NewMessageActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+
+
+    }
+}
