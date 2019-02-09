1715e5ffbe3a578c144f42ce75483b0405fefbcb
unknown <Treinamento@administrador8.DTRAINNING.local>
2019-02-09 15:17:51 -0200

    Notification

---
+++ others/notification/main/java/br/com/aluno/notificationapp/MainActivity.java
@@ -0,0 +1,51 @@
+package br.com.aluno.notificationapp;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.os.Bundle;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNotification();
+            }
+        });
+    }
+
+    private void addNotification() {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.notificationicon)
+                        .setContentTitle("NotificationApp")
+                        .setContentText("Essa é uma Notificação");
+
+        Intent notificationIntent = new Intent(this, NotificationActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Adiciona como Notificação
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+
+
+
+}
---
+++ others/notification/main/java/br/com/aluno/notificationapp/NotificationActivity.java
@@ -0,0 +1,12 @@
+package br.com.aluno.notificationapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NotificationActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
